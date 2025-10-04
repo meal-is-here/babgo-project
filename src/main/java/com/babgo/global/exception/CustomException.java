4c0ec4b71b73e7fae7d86c6fd1629c5255983659
Hojeong016 <skflsehdn@naver.com>
2025-10-04 17:33:38 +0900

    feat: customMessage 작성을 위한 필드 추가

--- src/main/java/com/babgo/global/exception/CustomException.java
+++ src/main/java/com/babgo/global/exception/CustomException.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 public class CustomException extends RuntimeException {
 
 	private final ErrorCode errorCode;
+	private final String customMessage;
 
-	public CustomException(ErrorCode errorCode) {
+	public CustomException(ErrorCode errorCode, String customMessage) {
 		super(errorCode.getMessage());
 		this.errorCode = errorCode;
+		this.customMessage = customMessage;
 	}
 }
