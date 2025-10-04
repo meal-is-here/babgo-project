d2fc99369e1556e6c9063e71a26a48a90c5cbb79
Hojeong016 <skflsehdn@naver.com>
2025-10-04 17:33:57 +0900

    feat: INVALID 공통 에러 코드 추가

--- src/main/java/com/babgo/global/exception/ErrorCode.java
+++ src/main/java/com/babgo/global/exception/ErrorCode.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
+	INVALID(HttpStatus.BAD_REQUEST, "올바르지 않은 값입니다."),
 	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드입니다."),
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청 형식이 올바르지 않습니다."),
 	NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 경로를 찾을 수 없습니다."),
