005bf07c13257a38afb3a86cc5507d2384d5cf70
naminsu <naminsoo1020@naver.com>
2025-10-05 15:36:03 +0900

    feat: 가게 수정 api
    - 가게 수정 api 추가
    - @Validated로 Create, Update 분리
    - 하나의 request로 Create, Update를 사용하기 위해 Upsert로 네이밍 변경
    -
    
    #16

--- src/main/java/com/babgo/controller/store/StoreController.java
+++ src/main/java/com/babgo/controller/store/StoreController.java
@@ -2,12 +2,15 @@ package com.babgo.controller.store;
 
 import com.babgo.application.store.StoreFacade;
 import com.babgo.global.api.ApiResponse;
-import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static com.babgo.controller.store.ValidateGroups.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +20,14 @@ public class StoreController {
     private final StoreFacade storeFacade;
 
     @PostMapping
-    public ApiResponse<Void> createStore(@Valid @RequestBody StoreRequest.Create storeRequest) {
-        storeFacade.createStore(storeRequest.toStoreInfo());
+    public ApiResponse<Void> createStore(@Validated({OnCreate.class, Default.class}) @RequestBody StoreRequest.Upsert storeRequest) {
+        storeFacade.createStore(storeRequest.toCreateInfo());
         return ApiResponse.success("가게 등록을 성공했습니다.");
     }
 
+    @PatchMapping("/{storeId}")
+    public ApiResponse<Void> updateStore(@PathVariable UUID storeId, @Validated({OnUpdate.class, Default.class}) @RequestBody StoreRequest.Upsert storeRequest) {
+        storeFacade.updateStore(storeId, storeRequest.toUpdateInfo());
+        return ApiResponse.success("가게 수정을 성공했습니다.");
+    }
 }
