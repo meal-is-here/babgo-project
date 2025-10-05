cf68ad20b7995c310a5f4b1aa549c88a2bae0db7
naminsu <naminsoo1020@naver.com>
2025-10-05 15:41:37 +0900

    feat: 가게 수정 전 해당 가게 찾기
    - 가게 수정 전, 수정을 위한 해당 가게 찾는 메서드 추가
    
    #16

--- src/main/java/com/babgo/domain/store/StoreRepository.java
+++ src/main/java/com/babgo/domain/store/StoreRepository.java
@@ -1,5 +1,9 @@
 package com.babgo.domain.store;
 
+import java.util.UUID;
+
 public interface StoreRepository {
     Store save(Store store);
+
+    Store findByStoreId(UUID storeId);
 }
--- src/main/java/com/babgo/repository/store/StoreRepositoryImpl.java
+++ src/main/java/com/babgo/repository/store/StoreRepositoryImpl.java
@@ -2,8 +2,12 @@ package com.babgo.repository.store;
 
 import com.babgo.domain.store.Store;
 import com.babgo.domain.store.StoreRepository;
+import com.babgo.global.exception.CustomException;
+import com.babgo.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,5 +18,11 @@ public class StoreRepositoryImpl implements StoreRepository {
     @Override
     public Store save(Store store) {
         return storeJpaRepository.save(store);
+    }
+
+    @Override
+    public Store findByStoreId(UUID storeId) {
+        return storeJpaRepository.findById(storeId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
     }
 }
