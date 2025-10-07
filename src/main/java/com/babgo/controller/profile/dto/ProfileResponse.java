98f1f15203b41cbd2bcb2a737ce6fcb6c7d569ff
dain <0901ekdls@gmail.com>
2025-10-07 19:35:54 +0900

    feat: 프로필 조회 응답 DTO 추가
    - name, nickname, phoneNumber, isProfilePublic 필드 포함

---
+++ src/main/java/com/babgo/controller/profile/dto/ProfileResponse.java
@@ -0,0 +1,14 @@
+package com.babgo.controller.profile.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProfileResponse {
+
+    private final String name;
+    private final String nickname;
+    private final String phoneNumber;
+    private final Boolean inProfilePublic;
+}
