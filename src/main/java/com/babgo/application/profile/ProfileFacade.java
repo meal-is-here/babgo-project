fda6b0f750adeee4a61d4e26270629ea1df0add0
whlsls3377 <whlsls3377@DESKTOP-K6SESPO>
2025-10-08 16:19:59 +0900

    fix: ProfileController and ProfileFacade 유저타입 변경으로 인해 변경

--- src/main/java/com/babgo/application/profile/ProfileFacade.java
+++ src/main/java/com/babgo/application/profile/ProfileFacade.java
@@ -2,7 +2,6 @@ package com.babgo.application.profile;
 
 import com.babgo.controller.profile.dto.ProfileResponse;
 import com.babgo.domain.profile.ProfileService;
-import com.babgo.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +14,7 @@ public class ProfileFacade {
     private final ProfileService profileService;
 
     // get profile
-    public ProfileResponse getMyProfile(String userId) {
-        User user = profileService.getMyProfile(userId);
-        return ProfileResponse.builder()
-                .name(user.getName())
-                .nickname(user.getNickname())
-                .phoneNumber(user.getPhoneNumber())
-                .isProfilePublic(user.getIsProfilePublic())
-                .build();
+    public ProfileResponse getMyProfile(Long userId) {
+        return profileService.getMyProfile(userId);
     }
 }
