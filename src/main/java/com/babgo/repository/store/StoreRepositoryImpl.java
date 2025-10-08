f918c754468e4f2e5cbc0b74b0fd49dfedd86f77
naminsu <naminsoo1020@naver.com>
2025-10-08 18:03:43 +0900

    merge: 충돌로 인해 수정
    - StoreRepositoryImpl 충돌로 인해 수정
    
    #16

--- src/main/java/com/babgo/repository/store/StoreRepositoryImpl.java
+++ src/main/java/com/babgo/repository/store/StoreRepositoryImpl.java
@@ -7,6 +7,7 @@ import com.babgo.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -18,6 +19,16 @@ public class StoreRepositoryImpl implements StoreRepository {
     @Override
     public Store save(Store store) {
         return storeJpaRepository.save(store);
+    }
+
+    @Override
+    public List<Store> saveAll(List<Store> stores) {
+        return storeJpaRepository.saveAll(stores);
+    }
+
+    @Override
+    public List<Store> findByStoreIdIn(List<UUID> storeIds) {
+        return storeJpaRepository.findByStoreIdIn(storeIds);
     }
 
     @Override
