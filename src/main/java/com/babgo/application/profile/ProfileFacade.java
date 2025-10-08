73c8904a3dee91058d74233b2d47316c5b550233
whlsls3377 <whlsls3377@DESKTOP-K6SESPO>
2025-10-08 16:55:48 +0900

    fix: user info update and profile 메소드 수정

--- src/main/java/com/babgo/application/profile/ProfileFacade.java
+++ src/main/java/com/babgo/application/profile/ProfileFacade.java
@@ -2,6 +2,8 @@ package com.babgo.application.profile;
 
 import com.babgo.controller.profile.dto.ProfileResponse;
 import com.babgo.domain.profile.ProfileService;
+import com.babgo.domain.user.User;
+import com.babgo.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ProfileFacade {
 
+    private final UserService userService;
     private final ProfileService profileService;
 
-    // get profile
+    /**
+     * 내 프로필 조회
+     */
     public ProfileResponse getMyProfile(Long userId) {
-        return profileService.getMyProfile(userId);
+        User user = userService.findByUserId(userId);
+        return ProfileResponse.from(user);
+    }
+
+    /**
+     * 프로필 수정
+     */
+    @Transactional
+    public void updateProfile(Long userId, String nickname, String phoneNumber, Boolean isProfilePublic) {
+        User user = userService.findByUserId(userId);
+        profileService.updateProfile(user, nickname, phoneNumber, isProfilePublic);
     }
 }
--- src/main/java/com/babgo/repository/user/UserRepository.java
+++ src/main/java/com/babgo/repository/user/UserRepository.java
@@ -14,6 +14,8 @@ public interface UserRepository {
 
     boolean existsByEmail(String email);
 
+    boolean existsByNickname(String nickname);
+
     Optional<User> findByUserIdAndDeletedAtIsNull(Long userId);
 
     void deleteAll();
