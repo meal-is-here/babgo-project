e03f9b14c2170ff25c3edd9f871fa78ea9e8341f
Hojeong016 <skflsehdn@naver.com>
2025-10-14 21:21:26 +0900

    refactor: 가상 pg 시물레이터 접근 권한

--- src/main/java/com/babgo/global/security/config/SecurityConfig.java
+++ src/main/java/com/babgo/global/security/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import com.babgo.global.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -86,8 +88,17 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // HTTP 요청 인가 규칙 설정
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/error").permitAll()
+                        // PG 시뮬/리다이렉트/웹훅
+                        .requestMatchers(
+                                "/pg-sim/**",
+                                "/v1/payments/success",
+                                "/v1/payments/fail",
+                                "/v1/payments/webhook",
+                                "/webhooks/**"
+                        ).permitAll()
                         // 인증 없이 접근 가능한 경로 (회원가입, 로그인)
-                        .requestMatchers("/api/auth/**","v1/orders/**").permitAll()
+                        .requestMatchers("/api/auth/**","/v1/orders/**","/v1/orders","/v1/payments/**").permitAll()
                         // 나머지 모든 요청은 인증 필요
                         .anyRequest().authenticated()
                 )
