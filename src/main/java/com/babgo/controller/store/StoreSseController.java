d95b49755a8b9b24b86c89ee2901302d267f3f13
naminsu <naminsoo1020@naver.com>
2025-10-17 05:35:23 +0900

    feat: 이벤트 발행
    - 이벤트 발행 관련 컨트롤러 추가(SSE)
    - 이벤트Info
    
    #37

--- src/main/java/com/babgo/controller/store/StoreSseController.java
+++ src/main/java/com/babgo/controller/store/StoreSseController.java
@@ -4,6 +4,7 @@ import com.babgo.repository.store.StoreSseHub;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -14,9 +15,8 @@ public class StoreSseController {
     private final StoreSseHub sseHub;
 
     @GetMapping(value = "/sse/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe() {
+    public SseEmitter subscribe(@RequestParam("userId") Long userId) {
         // TODO : @AuthenticationPrincipal 사용해서 userId값
-        Long userId = 1L;
         return sseHub.subscribe(userId);
     }
 }
