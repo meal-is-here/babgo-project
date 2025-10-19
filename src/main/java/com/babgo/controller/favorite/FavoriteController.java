6ed0329e74f1acbdcb27931b628ddf87f21ecd27
dain <0901ekdls@gmail.com>
2025-10-19 17:21:37 +0900

    feat: FavoriteController에 즐겨찾기 수정 API 추가
    - PATCH /v1/favorites/{favoriteId} 엔드포인트 구현
    - 인증은 임시로 userId 하드코딩 (TODO 주석 처리)
    - 수정 성공 시 200 OK + ApiResponse 반환
    
    #96

--- src/main/java/com/babgo/controller/favorite/FavoriteController.java
+++ src/main/java/com/babgo/controller/favorite/FavoriteController.java
@@ -1,16 +1,16 @@
 package com.babgo.controller.favorite;
 
-import com.babgo.controller.favorite.dto.FavoriteRequest;
+import com.babgo.controller.favorite.dto.FavoriteCreateRequest;
 import com.babgo.controller.favorite.dto.FavoriteResponse;
+import com.babgo.controller.favorite.dto.FavoriteUpdateRequest;
 import com.babgo.domain.favorite.FavoriteService;
 import com.babgo.global.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,12 +24,25 @@ public class FavoriteController {
     public ResponseEntity<ApiResponse<FavoriteResponse>> addFavorite(
             // TODO: 인증 추가 예정
             // @AuthenticationPrincipal Long userId,
-            @RequestBody FavoriteRequest request) {
+            @RequestBody FavoriteCreateRequest request) {
 
         Long userId = 1L;
         FavoriteResponse response = favoriteService.addFavorite(userId, request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.success("즐겨찾기가 등록되었습니다.", response));
+                .body(ApiResponse.success("즐겨찾기 등록 성공", response));
+    }
+
+    // update favorite
+    @PatchMapping("/{favoriteId}")
+    public ResponseEntity<ApiResponse<FavoriteResponse>> updateFavorite(
+            // TODO: 인증 추가 예정
+            // @AuthenticationPrincipal Long userId,
+            @PathVariable UUID favoriteId,
+            @RequestBody FavoriteUpdateRequest request
+    ) {
+        Long userId = 1L;
+        FavoriteResponse response = favoriteService.updateFavorite(userId, favoriteId, request);
+        return ResponseEntity.ok(ApiResponse.success("즐겨찾기 수정 성공c", response));
     }
 }
