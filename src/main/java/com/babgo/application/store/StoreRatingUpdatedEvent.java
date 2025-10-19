6cc9211aed361502a6eeb2bbf428a4132ef386dc
kimmyeongjin <kimmyeongjin@gmail.com>
2025-10-20 00:47:49 +0900

    remove: 사용하지 않는 이벤트 삭제

--- src/main/java/com/babgo/application/store/StoreRatingUpdatedEvent.java
+++
@@ -1,7 +0,0 @@
-package com.babgo.application.store;
-
-import java.util.UUID;
-
-public record StoreRatingUpdatedEvent(UUID storeId, UUID categoryId, String regionCode, double averageRatinge) {
-
-}
